61f11f9f8573f31dbb57841ccf748448cd35b5d3
Yevdokiia Mazhak <yevdokiia.mazhak@oag.com>
2025-06-19 18:36:49 +0300

    test: /api with github pages

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,6 +4,7 @@ axios.defaults.baseURL = 'https://pixabay.com';
 
 export const fetchImagesByQuery = async (searchedQuery, page = 1) => {
   const apiKey = '50853777-8fecba6f69a77bb52efd7171d';
+  // temp set it to `/api` to show the redirrect problem to http with github pages
 	const response = await axios.get("/api/", {
     params: {
       key: apiKey,
